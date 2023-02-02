d42f817d26c734aa977ba9f0e016f67511fb0bab
bin8727 <bin8727@naver.com>
2023-02-03 01:24:55 +0900

    업로드

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,104 @@
-import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled, { createGlobalStyle, keyframes, ThemeProvider } from 'styled-components';
+import { darkTheme, lightTheme } from "./theme";
+import {useRecoilValue, useSetRecoilState} from 'recoil';
+import { isDarkAtom } from "./atoms";
 
 function App() {
+  const isDark = useRecoilValue(isDarkAtom);
+  const setColor = useSetRecoilState(isDarkAtom);
+  const toggleChangeColor = () => {setColor((prev) => !prev)};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={isDark? darkTheme : lightTheme}>
+        <GlobalStyle />
+        <Container>
+          <Header>
+            <Img src={logo} />
+            <Toggle>
+              <input type='checkbox' onClick={toggleChangeColor} />
+              <span></span>
+            </Toggle>
+          </Header>
+        </Container>
+      </ThemeProvider>
+    </>
   );
 }
 
 export default App;
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    background-color: ${(props) => props.theme.bgColor}
+  }
+
+  code {
+    font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New',
+      monospace;
+  }
+`;
+const Container = styled.div`
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+`;
+const Header = styled.div`
+  background-color: ${(props) => props.theme.bgColor}
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+const Spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+const Img = styled.img`
+  height: 40vmin;
+  pointer-events: none;
+  @media (prefers-reduced-motion: no-preference) {
+    animation: ${Spin} infinite 100s linear;
+  }
+`;
+const Toggle = styled.label`
+  position: relative;
+  width: 60px;
+  height: 34px;
+  span {
+    position: absolute;
+    inset: 0;
+    background-color: gray;
+    transition: 0.4s;
+    border-radius: 34px;
+    &::before {
+      position: absolute;
+      content: "";
+      height: 26px;
+      width: 26px;
+      left: 4px;
+      bottom: 4px;
+      background-color: white;
+      transition: 0.5s;
+      border-radius: 50%;
+    }
+  }
+  input {
+    display: none;
+    &:checked + span {
+      background-color: black;
+    }
+    &:checked + span::before {
+      transform: translateX(26px);
+    }
+  }
+`;
